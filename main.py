6a122c4cfd212e27c47b10d5ab5f45d4a470c579
JessieMFox <95918500+JessieMFox@users.noreply.github.com>
2023-04-09 21:01:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import os
-import schedule
 import threading
 import time
+import subprocess
+
+subprocess.check_call(['pip', 'install', 'schedule'])
+
+import schedule
 
 
 def job():
   os.system("curl \
     -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36' \
-    -i https://sitory.eerfin.repl.co")
+    -i httpstodo")
 
 
-schedule.every(6).minutes.do(job)
+schedule.every(7).minutes.do(job)
 
 
 class ScheduleThread(threading.Thread):
